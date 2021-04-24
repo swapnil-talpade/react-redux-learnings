5dbab969bcb220f917b12228c053e85f09943953
Swapnil Talpade <swapniltalpade41@gmail.com>
2021-04-24 16:25:45 +0530

    implemented mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 import ChocolateContainer from './components/ChocolateContainer'
+import ItemContainer from './components/ItemContainer'
 import { Provider } from 'react-redux'
 import NewCakeContainer from './components/NewCakeContainer';
 
@@ -11,6 +12,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <IceCreamContainer />
         <HooksCakeContainer />
         <CakeContainer />
