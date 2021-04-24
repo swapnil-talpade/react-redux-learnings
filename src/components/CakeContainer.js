894b19db293e350bf6b7b3d68fa819eafc51a779
Swapnil Talpade <swapniltalpade41@gmail.com>
2021-04-24 13:07:24 +0530

    connected redux with reactapp

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { buyCake } from '../redux/index'
+import { connect } from 'react-redux'
+
+function CakeContainer(props) {
+    return (
+        <div>
+            <h2>Number of cakes  {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>buy cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
+
